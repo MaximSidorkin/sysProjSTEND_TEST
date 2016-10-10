f2231662a32e41c044edf411b65a7a48a492fa7b
MaximSidorkin <sidorkin.maxim@yahoo.com>
2016-10-10 17:43:34 +0300

    + patch 1010 for 004 (II)

--- 004_STAND_testNPA.py
+++ 004_STAND_testNPA.py
@@ -17,6 +17,7 @@ class ASeleniumLogin_1(unittest.TestCase):
     def test_1LoginInEORDev(self):
         assert "Login" in driver.title
         #wait = WebDriverWait(driver, 10)
+        time.sleep(3)
         _ = wait.until(EC.element_to_be_clickable((By.ID, 'LoginForm_username')))
         elem = driver.find_element_by_id("LoginForm_username")
         elem.send_keys("Ipad")
