e25518029f14c62d64c93e7b971b05924cfa67b8
MaximSidorkin <sidorkin.maxim@yahoo.com>
2017-03-28 18:38:54 +0300

    + downgrade ++ selenium 2.53.1 ++ Chromedriver

--- 005_STAND_testMeeting.py
+++ 005_STAND_testMeeting.py
@@ -8,7 +8,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
-
+#
 # global variable
 driver = webdriver.Chrome()
 #driver = webdriver.Firefox()
